72f25bdc86d011f688db8506cf1a4e77c4928823
dtaylor0128 <dtaylor0128@gmail.com>
2025-01-29 13:16:02 -0800

    On phase 1 I was able to install and test the API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-// backend/routes/index.js
-// ...
+
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -12,6 +12,11 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 // ...
 
 module.exports = router;
