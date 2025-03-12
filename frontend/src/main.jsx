07e15fe04cb25a5620ddfb4536957649e3a2cf9d
rmillay <rmmillay@gmail.com>
2025-03-11 17:48:18 -0700

    2nd update on set up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
 
 const store = configureStore();
 
-window.store = store;
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
